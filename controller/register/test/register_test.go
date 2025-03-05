477406aaef9244d0dd5e2e0770a3082e422c4ee5
nagaraj poojari <116948655+nagarajRPoojari@users.noreply.github.com>
2025-03-05 10:54:37 +0530

    config (#7)
    Co-authored-by: nagaraj-poojari <nagaraj.poojari@ibm.com>

---
+++ controller/register/test/register_test.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/nanoDFS/Slave/controller/register"
+)
+
+func TestRegister(t *testing.T) {
+	registerer := register.NewRegister(":9800")
+	if err := registerer.Register(); err != nil {
+		t.Errorf("%v", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/charmbracelet/log"
+import (
+	"fmt"
+
+	"github.com/nanoDFS/Slave/controller/register"
+	"github.com/nanoDFS/Slave/server/monitor"
+)
 
 func main() {
-	log.Info("Hello, wellcome")
+	registerer := register.NewRegister(":9800")
+	if err := registerer.Register(); err != nil {
+		fmt.Printf("%v", err)
+	}
+
+	monitor, _ := monitor.NewMonitorServerRunner(":9800")
+	monitor.Listen()
+
+	select {}
 }
