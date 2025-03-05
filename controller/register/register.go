4cceb855d061c7fd0816a83a21aec5588b009dbc
nagaraj-poojari <nagaraj.poojari@ibm.com>
2025-03-05 10:44:14 +0530

    config

--- controller/register/register.go
+++ controller/register/register.go
@@ -1,8 +1,36 @@
 package register
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/charmbracelet/log"
+	cs_pb "github.com/nanoDFS/Slave/controller/register/proto"
+	"github.com/nanoDFS/Slave/utils/config"
+	"google.golang.org/grpc"
+)
+
 type RegisterChunkServer struct {
+	masterAddr string
+	healthAddr string
 }
 
-func NewRegister() *RegisterChunkServer {
-	return &RegisterChunkServer{}
+func NewRegister(healthAddr string) RegisterChunkServer {
+	addr := config.LoadConfig().Master.Addr
+	return RegisterChunkServer{masterAddr: addr, healthAddr: healthAddr}
+}
+
+func (t RegisterChunkServer) Register() error {
+	conn, err := grpc.NewClient(t.masterAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Errorf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := cs_pb.NewChunkServerRegisterServiceClient(conn)
+	resp, err := client.Register(context.Background(), &cs_pb.ChunkServerRegisterReq{Address: t.healthAddr, Space: 10000})
+	if err != nil || !resp.Status {
+		return fmt.Errorf("failed to register: %v", err)
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/charmbracelet/log v0.4.0
 	google.golang.org/grpc v1.70.0
 	google.golang.org/protobuf v1.36.5
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
