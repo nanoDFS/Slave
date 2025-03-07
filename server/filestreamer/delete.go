d2a0eabb7fa5c19e76147f7f6743fa64624ea045
nagaraj poojari <116948655+nagarajRPoojari@users.noreply.github.com>
2025-03-07 23:13:38 +0530

    delete service (#11)
    Co-authored-by: nagaraj-poojari <nagaraj.poojari@ibm.com>

--- server/filestreamer/delete.go
+++ server/filestreamer/delete.go
@@ -2,12 +2,30 @@ package filestreamer
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/nanoDFS/Slave/controller/auth"
+	"github.com/nanoDFS/Slave/filesystem"
 	fs "github.com/nanoDFS/Slave/server/filestreamer/proto"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (t Server) Delete(ctx context.Context, req *fs.DeleteReq) (*fs.DeleteRes, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
+	token, err := ReadMetadata(ctx, "auth")
+	claim, ok := auth.NewAuth().AuthorizeDelete(token)
+
+	if err != nil || !ok {
+		return nil, fmt.Errorf("failed to authorize %v", err)
+	}
+
+	ChunkId, err := ReadMetadata(ctx, "chunk_id")
+	if err != nil {
+		return nil, fmt.Errorf("invalid chunk id")
+	}
+
+	fileSystem := filesystem.NewFileSystem("./test_root")
+	err = fileSystem.Delete(filesystem.FileOpts{FileId: claim.FileId, ChunkId: ChunkId})
+	if err != nil {
+		return nil, err
+	}
+	return &fs.DeleteRes{Status: true}, nil
 }
